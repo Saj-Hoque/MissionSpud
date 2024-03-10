cce048f8c3ebe3aea45c6b2d5868f61827722c1d
Sazidul Hoque <sajuss01@gmail.com>
2024-03-10 19:15:55 +0000

    Revert "add basic interaction with doorway to change scene"
    This reverts commit 678ef6f89cdd76a7e03127c3474f7e722295e5de.

--- World/world.gd
+++ World/world.gd
@@ -6,8 +6,6 @@ extends Node2D
 @onready var scrapAreaOrigin = scrapArea.global_position - scrapAreaDims
 var scrap_scene = preload("res://Resources/scrap.tscn")
 
-@onready var doorway = get_node("/root/world/doorway")
-
 func _ready():
 	Global.potatoCount = 0
 	Global.scrapCount = 0
@@ -27,10 +25,5 @@ func _on_scrap_spawn_timer_timeout():
 	if scraps.get_child_count() < Global.max_scraps:
 		var scrap = scrap_scene.instantiate()
 		scrap.position = Vector2(randi_range(scrapAreaOrigin.x, scrapAreaOrigin.x + (scrapAreaDims.x*2)), randi_range(scrapAreaOrigin.y, scrapAreaOrigin.y + (scrapAreaDims.y*2)))
-		scraps.add_child(scrap)
-		
-func _input(event):
-	if event.is_action_pressed("interact"):
-		if doorway.get_overlapping_bodies().size() > 0:
-			get_tree().change_scene_to_file("res://Menu/menu.tscn")
+		scraps.add_child(scrap) 
 	
