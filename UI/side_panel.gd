3db40fdae4709c27e5ca134751be67d3172d4765
Sazidul Hoque <sajuss01@gmail.com>
2024-03-19 14:13:17 +0000

    fixed issue with not updating status

--- UI/side_panel.gd
+++ UI/side_panel.gd
@@ -4,10 +4,7 @@ extends CanvasLayer
 @onready var state = $Panel/State
 @onready var first_name = $Panel/Name
 
-var on_screen = false
-
 func appear():
-	on_screen = true
 	visible = true
 	anim.play("TransIn")
 	await anim.animation_finished
@@ -16,10 +13,9 @@ func appear():
 func dissappear():
 	anim.play("TransOut")
 	await anim.animation_finished
-	on_screen = false
 	
 func _process(delta):
-	if on_screen and not SelectionManager.current_selection.is_empty():
+	if not SelectionManager.current_selection.is_empty():
 		state.text = SelectionManager.current_selection[0].status
 		first_name.text = SelectionManager.current_selection[0].first_name
 
