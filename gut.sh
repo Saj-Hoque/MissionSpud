38b7ebd1a481d3aeb24687878139aa033fbd00a2
Sazidul Hoque <sajuss01@gmail.com>
2024-05-02 18:54:46 +0100

    update gut.sh

--- gut.sh
+++ gut.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 
-echo "  ▄████  █    ██ ▄▄▄█████▓   ▄▄▄█████▓▓█████   ██████ ▄▄▄█████▓  ██████ ";
-echo " ██▒ ▀█▒ ██  ▓██▒▓  ██▒ ▓▒   ▓  ██▒ ▓▒▓█   ▀ ▒██    ▒ ▓  ██▒ ▓▒▒██    ▒ ";
-echo "▒██░▄▄▄░▓██  ▒██░▒ ▓██░ ▒░   ▒ ▓██░ ▒░▒███   ░ ▓██▄   ▒ ▓██░ ▒░░ ▓██▄   ";
-echo "░▓█  ██▓▓▓█  ░██░░ ▓██▓ ░    ░ ▓██▓ ░ ▒▓█  ▄   ▒   ██▒░ ▓██▓ ░   ▒   ██▒";
-echo "░▒▓███▀▒▒▒█████▓   ▒██▒ ░      ▒██▒ ░ ░▒████▒▒██████▒▒  ▒██▒ ░ ▒██████▒▒";
-echo " ░▒   ▒ ░▒▓▒ ▒ ▒   ▒ ░░        ▒ ░░   ░░ ▒░ ░▒ ▒▓▒ ▒ ░  ▒ ░░   ▒ ▒▓▒ ▒ ░";
-echo "  ░   ░ ░░▒░ ░ ░     ░           ░     ░ ░  ░░ ░▒  ░ ░    ░    ░ ░▒  ░ ░";
-echo "░ ░   ░  ░░░ ░ ░   ░           ░         ░   ░  ░  ░    ░      ░  ░  ░  ";
-echo "      ░    ░                             ░  ░      ░                 ░  ";
-echo "                                                                        ";
-
 LANG="en_US.utf8"
-GODOT_VERSION=3.2.2
+GODOT_VERSION=4.2.1
 GODOT_BIN=Godot_v${GODOT_VERSION}-stable_linux_headless.64
 GODOT_ZIP=${GODOT_BIN}.zip
 GODOT_URL=https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/${GODOT_ZIP}
@@ -24,4 +13,4 @@ fi
 curl -s -S ${GODOT_URL} --output ${GODOT_ZIP}
 unzip -q ${GODOT_ZIP}
 chmod +x ${GODOT_BIN}
-./${GODOT_BIN} -d -s --path $GITHUB_WORKSPACE addons/gut/gut_cmdln.gd -gdir=res://test -ginclude_subdirs -gexit
+./${GODOT_BIN} -d -s --path $GITHUB_WORKSPACE addons/gut/gut_cmdln.gd -gdir=res://Unit_Tests -ginclude_subdirs -gexit
